1ec0aa67e151450dd8b8e8b90309fe5438f1e4c3
Arhip13 <kakashihatake6hokagegygg@gmail.com>
2024-04-20 20:30:41 +0300

    почали робит бургер меню

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const burger = document.getElementById('burger')
-const nav = document.getElementById('nav-ul')
+const nav = document.getElementById('mobile-menu')
 burger.addEventListener('click',function(){
     nav.classList.toggle('show')
 })
