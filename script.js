bd12d6025c2d940b8ac7396b318942a78d93cf31
Arhip13 <kakashihatake6hokagegygg@gmail.com>
2024-04-21 20:22:08 +0300

    Дороблюємо бургер меню

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const burger = document.getElementById('burger')
 const nav = document.getElementById('mobile-menu')
+const burger = document.getElementById('burger')
+const header = document.getElementById('header')
+const mobileBurger = document.getElementById('mobile-burger')
 burger.addEventListener('click',function(){
     nav.classList.toggle('show')
+    header.classList.toggle('show')
+})
+
+mobileBurger.addEventListener('click', function(){
+    nav.classList.toggle('ul-hidden')
+    header.classList.toggle('ul-hidden')
 })
