c89ac17775c7e0ca070b6526d8479243d48913ca
Arhip13 <kakashihatake6hokagegygg@gmail.com>
2024-05-03 17:42:50 +0300

    23242424

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const nav = document.getElementById('mobile-menu')
 const burger = document.getElementById('burger')
 const header = document.getElementById('header')
-const mobileBurger = document.getElementById('mobile-burger')
+const mobileBurger = document.getElementById('mobile-burger-menu-top-burger-mobile')
+
 burger.addEventListener('click',function(){
     nav.classList.toggle('show')
     header.classList.toggle('show')
 })
 
 mobileBurger.addEventListener('click', function(){
-    nav.classList.toggle('ul-hidden')
-    header.classList.toggle('ul-hidden')
+    nav.classList.toggle('show')
+    header.classList.toggle('show')
 })
